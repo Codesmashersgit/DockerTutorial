96917d687080ca10d419a50bb05c3baa3123d398
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2026-02-25 01:52:23 +0530

    Add styling on jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import './App.css'
 function App() {
   return (
-    <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh"}}>App</div>
+    <div style={{position:"fixed",display:"flex", justifyContent:"center", alignItems:"center", backgroundColor:"beige", height:"100vh", color:"white", width:"100vw"}}>
+      <h1 style={{fontSize:"70px", color:"black"}}>Welcome to Docker Tutorial</h1>
+    </div>
   )
 }
 
